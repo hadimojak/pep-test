e26952b0f992a49013a095851904d528e3204125
hadi arbabi <hadimojak@gmail.com>
2024-04-11 00:12:43 +0330

    first init

---
+++ server.js
@@ -0,0 +1,27 @@
+const {consola} = require("consola");
+const express = require("express");
+const Port = 3000;
+const app = express();
+const server = require("http").createServer(app);
+
+const fs = require("fs");
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  console.log("create");
+  return res.send(["a", "n"]);
+});
+
+app.get("/", (req, res) => {
+  console.log("inquery");
+
+  return res.send("OK");
+});
+
+server.listen(Port, (req, res) => {
+    consola.success({ message: `server is starting on http://localhost:${Port}/graphql`, badge: true });
+});
