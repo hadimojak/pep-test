480a746d73c53ae4f7d6a613c7c86d45907cf420
hadi arbabi <hadimojak@gmail.com>
2024-04-13 03:30:15 +0330

    last commit

--- server.js
+++ server.js
@@ -4,22 +4,6 @@ const Port = 3000;
 const app = express();
 const server = require("http").createServer(app);
 const { Sequelize, DataTypes, Model, Op } = require("sequelize");
-
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.post("/", (req, res) => {
-  console.log("create");
-  return res.send(["a", "n"]);
-});
-
-app.get("/", (req, res) => {
-  console.log("inquery");
-  return res.send("OK");
-});
-
 const connection = {
   host: "localhost",
   dialect: "mysql",
@@ -38,41 +22,81 @@ const Square = sequelize.define(
   "Square",
   {
     x: {
-      type: DataTypes.NUMBER,
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
     y: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
     width: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
     height: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
-
-    createdAt: Sequelize.DATE,
-    updatedAt: Sequelize.DATE,
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: Sequelize.NOW,
+    },
   },
   {
     sequelize: sequelize,
     freezeTableName: true,
     modelName: "square",
+    timestamps: false,
   }
 );
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post("/", async (req, res) => {
+  let count = 0;
+  const { main: mainSquare, input } = req.body;
+  const squareWidth = [+mainSquare.x, +mainSquare.x + +mainSquare.width];
+  const squareHeight = [+mainSquare.y, +mainSquare.y + +mainSquare.height];
+
+  const validSquareArray = input.filter((val) => {
+    if (val.x > squareWidth[1] || val.y > squareHeight[1]) return false;
+    if (val.x < squareWidth[1] && val.x > squareWidth[0]) return true;
+    if (val.y < squareHeight[1] && val.y > squareHeight[0]) return true;
+    if (val.x < squareWidth[0] && val.width > squareWidth[0]) return true;
+    if (val.y < squareHeight[0] && val.height > squareHeight[0]) return true;
+    if (val.x < squareWidth[0] && val.width < squareWidth[0]) return false;
+    if (val.y < squareHeight[0] && val.height < squareHeight[0]) return false;
+  });
+
+  if (validSquareArray.length) {
+    for (const item of validSquareArray) {
+      await Square.create({ x: item.x, y: item.y, width: item.width, height: item.height }).then(() => {
+        count = count + 1;
+      });
+    }
+  }
+  return res.send(`${count} number of square added to db`);
+});
+app.get("/", async (req, res) => {
+  const squares = await Square.findAll();
+  return res.send(
+    squares.map(function (item) {
+      return item["dataValues"];
+    })
+  );
+});
+
 //connect to db
 sequelize
-  .sync({ alter: false })
+  .sync({ alter: true })
   .then(() => {
     console.log("db connected");
   })
   .then(() => {
     server.listen(Port, () => {
-      consola.success({ message: `server is starting on http://localhost:${Port}/graphql`, badge: true });
+      consola.success({ message: `server is starting on http://localhost:${Port}`, badge: true });
     });
   })
   .catch((err) => {
